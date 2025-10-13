00555269e2fdb99b603a3b716a7ff09faf14f29a
altyt <qextzy@gmail.com>
2025-10-13 11:57:56 +0200

    modification seed

--- database/seed.sql
+++ database/seed.sql
@@ -3,11 +3,11 @@
 -- Insertion people --
 INSERT INTO people(email, password_hash, name, phone, status) VALUES
 -- Admin --
-('admin@jobboard.com', 'admin123456', 'Admin Principal', '0601020304', 'admin'),
+('admin@jobboard.com', '$2y$10$HELAkYRZTUCTK9mmv8xDiedX2nXeJSIdtmB5Ud/r1DypLSJ4d/8AW', 'Admin Principal', '0601020304', 'admin'),
 
 -- Companies --
-('contact@techcorp.fr', 'company123456', 'Marie Dubois', '0612345678', 'company'),
-('rh@innovsolutions.fr', 'company123456', 'Pierre Martin', '0623456789', 'company'),
+('contact@techcorp.fr', '$2y$10$HELAkYRZTUCTK9mmv8xDiedX2nXeJSIdtmB5Ud/r1DypLSJ4d/8AW', 'Marie Dubois', '0612345678', 'company'),
+('rh@innovsolutions.fr', '$2y$10$HELAkYRZTUCTK9mmv8xDiedX2nXeJSIdtmB5Ud/r1DypLSJ4d/8AW', 'Pierre Martin', '0623456789', 'company'),
 
 -- Candidate --
 ('jean.dupont@email.fr', 'people123456', 'Jean Dupont', '0645678901', 'candidate');
